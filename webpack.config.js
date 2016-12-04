972bb68566cfd2d251253da168f8dd5d72edca50
feilong.cao <feilong.cao@fengjr.com>
2016-12-04 15:59:05 +0800

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
-module.exports = {
-    entry: "./app/index.js",
-    output: {
-        path: `${__dirname}/ref/`,
-        filename: '[name].js',
-        publicPath: '/assets/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js|jsx$/, loader: 'babel',
-                exclude: /(node_modules|bower_components)/,
-                query: {
-                    presets: ['es2015', 'react']
+
+module.exports = function (config) {
+    return {
+        entry: "./app/index.js",
+        output: {
+            path: `${__dirname}/ref/`,
+            filename: '[name].js',
+            publicPath: config.publicPath
+        },
+        module: {
+            loaders: [
+                {
+                    test: /\.js|jsx$/, loader: 'babel',
+                    exclude: /(node_modules|bower_components)/,
+                    query: {
+                        presets: ['es2015', 'react']
+                    }
                 }
-            }
-        ]
-    },
-    devServer: {
-        contentBase: "ref/"
-    }
-};
+            ]
+        },
+        devServer: {
+            contentBase: "ref/"
+        }
+    };
+}
