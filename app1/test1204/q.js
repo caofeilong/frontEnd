0ba260f037c16f4535efd65f02abb44f6b0c8d38
sunyubk <18709719642@qq.com>
2016-12-18 18:32:17 +0800

    广告轮播

--- app1/test1204/q.js
+++ app1/test1204/q.js
@@ -1,23 +1,34 @@
 $(document).ready(function () {
 
-
+    var timer = null;
     var index = 0;
 
     var mg = $("#focus img");
 
+    var nvbs = $("#navBra a");
+
     var len = mg.length;
 
-    function play(n) {;
-        mg.eq(n).fadeIn(200).siblings("img").fadeOut(200);
+    function play(n) {
+
+
+        mg.eq(n).fadeIn(1000).siblings("img").fadeOut(1000);
+
+        nvbs.eq(n).addClass('active').siblings("a").removeClass('active');
+        //addClass('')          添加样式
+        //removeClass('')       移除样式        移除它本身之外的元素。。
     }
 
-    // eq.(n)   取得mg中的值
+    // eq.(n)   取得mg中的值  n为下标。
     //fadeIn(200)   渐入（200）  用200毫秒渐入
-    //fadeOut(200)  渐出（200）  用200毫秒渐出
+    //fadeOut(200)  渐出（200）  用200毫秒渐它本身之外的元素。
+    //siblings(" ")  除了我之外的，我的兄弟姐妹
+
+
 
 
     //setInterval()  每隔2000毫秒都会执行一次    循环执行
-    setInterval(function () {
+    timer=setInterval(function () {
         index++;
         play(index);
         if (index == len) {        //如果index的值等于mg.length数组的长度，是使index归零
@@ -42,4 +53,22 @@ $(document).ready(function () {
         }
         play(index);
     });
+
+
+    $("#focus").hover(
+        function(){
+            clearInterval(timer);
+        },function(){
+            timer= setInterval(function () {
+                index++;
+                play(index);
+                if (index == len) {        //如果index的值等于mg.length数组的长度，是使index归零
+                    index = 0;
+                }
+            }, 2000);
+        }
+    )
 })
+
+
+
