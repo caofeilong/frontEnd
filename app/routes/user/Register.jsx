b60866ff7dc9dfd3a445331d931171ff456cef6b
feilong.cao <feilong.cao@fengjr.com>
2017-02-15 20:00:16 +0800

    fox

--- app/routes/user/Register.jsx
+++ app/routes/user/Register.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import  $   from 'jquery';
 
-class Home extends Component {
+className Home extends Component {
+
   register() {
     $.post('/api/user/register', {
       "uname": "本地测试",
@@ -11,11 +12,26 @@ class Home extends Component {
     })
   }
 
-  render() {
+  render(){
     return (
-      <div >
-        register
-        <button onClick={this.register}>注册</button>
+      <div className="row" style={{margin:"auto",width:300,"marginTop":50}}>
+
+        <div className="form-group">
+          <label for="inputEmail3" className="col-sm-2 control-label">账号</label>
+          <div className="col-sm-10">
+            <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label for="inputPassword3" className="col-sm-2 control-label">密码</label>
+          <div className="col-sm-10">
+            <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+          </div>
+        </div>
+
+        <button className="btn btn-primary" style={{margin:"auto",width:100,display:'block'}} onClick={this.register}>
+          注册
+        </button>
       </div>
     )
   }
